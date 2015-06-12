55ce788047f609811ff8c786497897b90cb5fd6e
Baptiste Boullenger <wirder@gmail.com>
2015-06-12 15:04:02 +0200

    Ajout du client

---
+++ client/article/article.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.article', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/article/:id', {
+    templateUrl: 'article/article.html',
+    controller: 'ArticleCtrl'
+  });
+}])
+
+.controller('ArticleCtrl', [function() {
+
+}]);
